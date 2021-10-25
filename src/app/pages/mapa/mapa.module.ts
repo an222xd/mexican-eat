c987ed715700ba7cb3b6bd6acb0b870604e176f6
an222xd <joseantonio222xd@gmail.com>
2021-10-25 18:26:03 -0500

    mi primer commit

---
+++ src/app/pages/mapa/mapa.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapaComponent } from './mapa.component';
+
+
+
+@NgModule({
+  declarations: [
+    MapaComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MapaModule { }
